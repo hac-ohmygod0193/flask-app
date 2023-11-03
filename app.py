f17aa8015b2a11575b39d63b88d4fd2ab74bbcd0
hengan cheng <alex.chenghengan@gmail.com>
2023-11-03 13:31:08 +0800

    test

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import flask
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('test.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
